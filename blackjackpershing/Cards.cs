1b170dd5a9b4f76e5e24ad19c1324689350ca226
Bryan Sory <bryansory@icloud.com>
2016-10-12 17:58:24 -0500

    added shuffle and improved modules

--- blackjackpershing/Cards.cs
+++ blackjackpershing/Cards.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace blackjackpershing
 {
-    public enum Suits {
+    public enum Suits
+    {
         Hearts, Clubs, Spades, Diamonds
     }
     public enum Ranks
     {
         Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
     }
-    
+
     public class Card
     {
         public Suits suits;
@@ -24,15 +25,54 @@ namespace blackjackpershing
             ranks = r;
         }
     }
+    public class Deck
+    {
+        public Card[] cards;
+        public Deck()
+        {
+            
+            cards = new Card[52];
+            int i = 0; // 1
+            foreach (Ranks r in Enum.GetValues(typeof(Ranks)))
+            {
+                int j = 0;
+                foreach (Suits s in Enum.GetValues(typeof(Suits)))
+                {
+                    cards[i * 4 + j] = new Card(s, r);
+                    j++;
+                }
+                i++;
+            }
+            randomize(cards);
+        }
+        //public override string ToString(Ranks r, Suits s)
+        //{
 
-    //public class Deck
-    //{
-    //    Cards deckCards = new Cards();
-    //    for (int i = 0; i<deckCards; i++) 
-    //}
+        //}
+        public Card[] randomize(Card[] shuffledDeck)
+        {
+            //Card[] shuffledDeck = new Card[52];
+            Random x = new Random();
+            Card temp;
+            int a;
 
-    //class Hand
-    //{
+            for (int i = 0; i < 52; i++)
+            {
+                a = x.Next(0, 52);
+                temp = shuffledDeck[a];
+                shuffledDeck[i] = shuffledDeck[a];
+                temp = shuffledDeck[0];
+            }
+            return shuffledDeck;
 
-    //}
-}
+        }
+    }
+
+        
+
+
+        //class Hand
+        //{
+
+        //}
+    }
--- blackjackpershing/Program.cs
+++ blackjackpershing/Program.cs
@@ -8,70 +8,76 @@ namespace blackjackpershing
 {
     class Program
     {
-        
-        List<Card> playerOne = new List<Card>(); //add cards to list
-        List<Card> playerTwo = new List<Card>();
-        List<Card> cards = new List<Card>();
-        int counter=0;
 
         static void Main(string[] args)
         {
-            Program myProgram = new Program();
-            myProgram.createDeck();
-            myProgram.printDeck();
+            //List<Card> playerOne = new List<Card>(); //add cards to list
+            //List<Card> playerTwo = new List<Card>();
 
-        }
-        public void createDeck()
-        {
+            //int counter = 0;
 
-            foreach (Ranks rank in Enum.GetValues(typeof(Ranks)))
-            {
-                Card Card = new Card(Suits.Hearts, rank);
-                Card card2 = new Card(Suits.Clubs, rank);
-                Card card3 = new Card(Suits.Spades, rank);
-                Card card4 = new Card(Suits.Diamonds, rank);
-                cards.Add(Card);
-                cards.Add(card2);
-                cards.Add(card3);
-                cards.Add(card4);
-
-            }
-        }
-         public void printDeck()
-        {
+            //Program myProgram = new Program();
+            //myProgram.createDeck();
+            //myProgram.printDeck();
+            //myProgram.randomize();
+            //myProgram.dealOne();
+            Deck firstDeck = new Deck();
             for (int i = 0; i < 52; i++)
             {
-                Console.WriteLine(cards.ElementAt(i).ranks);
-                Console.WriteLine(cards.ElementAt(i).suits);
+                Console.WriteLine(firstDeck.cards[i].ranks.ToString());
+                Console.WriteLine(firstDeck.cards[i].suits.ToString());
+            
             }
             Console.ReadLine();
         }
-        public void dealTwo()
-        {
-            Card card1 = cards.ElementAt(counter);
-            Card card2 = cards.ElementAt(counter+1);
-            counter = counter + 2;
-        }      
-        public void dealOne()
-        {
-            Card card1 = cards.ElementAt(counter);
-            counter = counter + 1;
-        }
-        public void deal(int cardCount)
-        {
-            counter = cardCount;
-            for (int i = 0; i < cardCount; i++)
-            {
-                cards.ElementAt(i);
-                counter = counter + 1;
-            }
+        //}
+        
+        //public void swap()
+        //{
+        //    List<Card> swapper = new List<Card>();
+        //}
+
+        //public void printDeck()
+        //{
+        //    for (int i = 0; i < 52; i++)
+        //    {
+        //Console.WriteLine(cards.ElementAt(i).ranks);
+        //Console.WriteLine(cards.ElementAt(i).suits);
+        //    }
+        //    Console.ReadLine();
+        //}
+        //public void dealTwo()
+        //{
+        //    Card card1 = cards.ElementAt(counter);
+        //    Card card2 = cards.ElementAt(counter + 1);
+        //    counter = counter + 2;
+        //}
+        //public void dealOne()
+        //{
+          
+        //    Console.WriteLine(cards[0].ranks);
+        //    Console.WriteLine(cards[0].suits);
+        //    counter = counter + 1;
+        //    Console.ReadLine();
+        //}
+        //public void deal(int cardCount)
+        //{
+        //    counter = cardCount;
+        //    for (int i = 0; i < cardCount; i++)
+        //    {
+        //        cards.ElementAt(i);
+        //        counter = counter + 1;
+        //    }
+
+        //}
+    }
+}
+
+
     
-    }
-        }
-        }
 
 
-    }
+    
 
  
 
